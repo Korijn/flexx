fa3d059de79e32017b75c3d4c2b74346624a87e0
Korijn van Golen <korijn@gmail.com>
2016-12-02 08:58:29 +0100

    Route construction appears to be working

--- flexx/ui/widgets/_router.py
+++ flexx/ui/widgets/_router.py
@@ -2,18 +2,54 @@ from flexx import ui, app
 
 
 class Router(app.Model):
-    pass
+    def init(self):
+        self.routes = {}
+        self._route_context_stack = [self]
+
+    def register(self, route):
+        parent = self._route_context_stack[-1]
+        parent.routes[route.path] = route
+
+    def push_route_context(self, route):
+        self._route_context_stack.append(route)
+
+    def pop_route_context(self):
+        self._route_context_stack.pop()
+
 
 class Route:
-    pass
+    def __init__(self, path, widget):
+        self.router = app.get_active_model()
+        if not isinstance(self.router, Router):
+            raise ValueError("All Route objects must be instantiated in the context of a Router.")
+
+        self.path = path
+        self.widget = widget
+        self.routes = {}
+
+        self.router.register(self)
+
+    def __enter__(self):
+        self.router.push_route_context(self)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.router.pop_route_context()
+
 
 class IndexRoute(Route):
-    pass
+    def __init__(self, widget):
+        super().__init__(None, widget)
+
 
 class Placeholder(ui.Widget):
     pass
 
+
 class Link(ui.Widget):
+    pass
+
+
+class Foo(ui.Widget):
     pass
 
 
@@ -22,8 +58,14 @@ if __name__ == '__main__':
 
     class MyApp(ui.Widget):
         def init(self):
-            with ui.Router():
-                pass
+            with Router():
+                with Route(path="/", widget=Foo):
+                    IndexRoute(widget=Foo)
+                    Route(path="about", widget=Foo)
+                    with Route(path="tasks", widget=Foo):
+                        Route(path="/:task-id", widget=Foo)
+                    Route(path="contact", widget=Foo)
+
 
     m = app.App(MyApp)
     m.launch('xul')
